a424c580caf32f4da048fb64f3c95fc394d450e8
DenisConsta <denisconstantin338@gmail.com>
2022-10-13 17:56:40 +0200

    [remove] functions

--- js/script.js
+++ js/script.js
@@ -10,23 +10,17 @@ for(let i=1; i<=limit; i++){
   container.append(box);
 
   // controlli di divisbilità
-  if(DivisibleBy(i, 3)){
+  if(!(i % 3)){
     box.classList.add("fizz");
     box.innerHTML = "fizz";
   }
-  if(DivisibleBy(i, 5)){
+  if(!(i % 5)){
     box.classList.add("buzz");
     box.innerHTML = "buzz";
   }
-  if(DivisibleBy(i, 3) && (DivisibleBy(i, 5))){
+  if((!(i % 3)) && (!(i % 5))){
     box.classList.add("fizzbuzz");
     box.innerHTML = "fizzbuzz";
   }
 }
 
-// funzione per stabilire se il valore è divisibile per un altro numero
-function DivisibleBy(num, div){
-  if(!(num % div))
-    return true;
-  return false;
-}
