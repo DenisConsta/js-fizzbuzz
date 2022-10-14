2d1ebf6e5f7865ad395e89a42182e5ff508d643c
DenisConsta <denisconstantin338@gmail.com>
2022-10-14 09:49:06 +0200

    [fix]

--- js/script.js
+++ js/script.js
@@ -6,21 +6,19 @@ for(let i=1; i<=limit; i++){
   // generazione del box
   const box = document.createElement('div');
   box.className = "box";
-  box.innerHTML = i;
   container.append(box);
 
   // controlli di divisbilità
-  if(!(i % 3)){
-    box.classList.add("fizz");
-    box.innerHTML = "fizz";
-  }
-  if(!(i % 5)){
-    box.classList.add("buzz");
-    box.innerHTML = "buzz";
-  }
-  if((!(i % 3)) && (!(i % 5))){
+  if(!(i % 15)){
     box.classList.add("fizzbuzz");
     box.innerHTML = "fizzbuzz";
-  }
+  }else if(!(i % 3)){
+    box.classList.add("fizz");
+    box.innerHTML = "fizz";
+  }else if(!(i % 5)){
+    box.classList.add("buzz");
+    box.innerHTML = "buzz";
+  }else
+    box.innerHTML = i;
 }
 
